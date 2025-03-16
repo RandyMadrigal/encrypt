b40d945c669885eaf3ff1a598f7477466e2a0386
Randy Madrigal <randymadrigalx12@gmail.com>
2025-03-16 18:59:41 -0400

    add-page: loading page

--- frontend/src/utils/ProtectedRoute.tsx
+++ frontend/src/utils/ProtectedRoute.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hook/useAuth";
+import { LoadingPage } from "../components/LoadingPage";
 
 export function ProtectedRoute() {
   const { isAuthenticated } = useAuth();
 
-  if (isAuthenticated === null) return <div>Loading...</div>;
+  if (isAuthenticated === null) return <LoadingPage />;
   return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 }
